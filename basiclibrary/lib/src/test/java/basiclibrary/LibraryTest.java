1b25439802bf7171c7184e16d76e5aa0054f916c
Jennifer Sung <sung.jenni93@gmail.com>
2023-07-11 22:19:46 -0400

    rolling dice task

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    @Test
+    void testRollMethod() {
+        Library library = new Library();
+        int[] rolls = library.roll(5);
+        assertEquals(5, rolls.length, "Array length should be 5");
     }
 }
